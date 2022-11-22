cba4c0f404e72d22ad7e394fa871f498b4ab80a2
sina.ardehali@gmail.com <sina.ardehali@gmail.com>
2022-11-23 00:16:08 +0100

    ajout dependance

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from annotated_text import annotated_text
+import spacy
 
 """
 # Welcome to Streamlit!
@@ -16,23 +18,87 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+#     Point = namedtuple('Point', 'x y')
+#     data = []
 
-    points_per_turn = total_points / num_turns
+#     points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
+
+
+@st.cache(show_spinner=False, allow_output_mutation=True, suppress_st_warning=True)
+def load_models():
+    # french_model = spacy.load("./models/fr/")
+    # english_model = spacy.load("./models/en/")
+    english_model = spacy.load("en_core_web_sm")
+    models = {"en": english_model}
+    # models = {"en": english_model, "fr": french_model}
+    return models
+
+
+def process_text(doc, selected_entities, anonymize=False):
+    tokens = []
+    for token in doc:
+        if (token.ent_type_ == "PERSON") & ("PER" in selected_entities):
+            tokens.append((token.text, "Person", "#faa"))
+        elif (token.ent_type_ in ["GPE", "LOC"]) & ("LOC" in selected_entities):
+            tokens.append((token.text, "Location", "#fda"))
+        elif (token.ent_type_ == "ORG") & ("ORG" in selected_entities):
+            tokens.append((token.text, "Organization", "#afa"))
+        else:
+            tokens.append(" " + token.text + " ")
+
+    if anonymize:
+        anonmized_tokens = []
+        for token in tokens:
+            if type(token) == tuple:
+                anonmized_tokens.append(("X" * len(token[0]), token[1], token[2]))
+            else:
+                anonmized_tokens.append(token)
+        return anonmized_tokens
+
+    return tokens
+
+
+models = load_models()
+
+selected_language = st.sidebar.selectbox("Select a language", options=["en", "fr"])
+selected_entities = st.sidebar.multiselect(
+    "Select the entities you want to detect",
+    options=["LOC", "PER", "ORG"],
+    default=["LOC", "PER", "ORG"],
+)
+selected_model = models[selected_language]
+
+text_input = st.text_area("Type a text to anonymize")
+
+uploaded_file = st.file_uploader("or Upload a file", type=["doc", "docx", "pdf", "txt"])
+if uploaded_file is not None:
+    text_input = uploaded_file.getvalue()
+    text_input = text_input.decode("utf-8")
+
+anonymize = st.checkbox("Anonymize")
+doc = selected_model(text_input)
+tokens = process_text(doc, selected_entities)
+
+annotated_text(*tokens)
+
+if anonymize:
+    st.markdown("**Anonymized text**")
+    st.markdown("---")
+    anonymized_tokens = process_text(doc, selected_entities, anonymize=anonymize)
+    annotated_text(*anonymized_tokens)
